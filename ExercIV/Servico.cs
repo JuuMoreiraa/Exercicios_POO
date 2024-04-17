d650e80a8bf014e7a0036157eee5e758416cccc6
Juliana Moreira <omoreiraju@gmail.com>
2024-04-17 20:40:25 -0300

    Ajuste da data

--- ExercIV/Servico.cs
+++ ExercIV/Servico.cs
@@ -2,5 +2,5 @@ public class Servico
 {
     public string Contratante { get; set; }
     public string Responsavel => "Sistema - Via aplicativo";
-    public string DataContrato => DateTime.Now.ToString("dd-MM-yyyy");
+    public string DataContrato => DateTime.Now.ToString("dd/MM/yyyy");
 }
--- ExercIV/obj/Debug/net8.0/ExercIV.AssemblyInfo.cs
+++ ExercIV/obj/Debug/net8.0/ExercIV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercIV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d235c770c3f71cee9c384601a15a8b80bad70bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7685fc8d211bb5a30ff497512db17023b97da9d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercIV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercIV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
